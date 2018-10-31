3df0f7043698ce628663b99b47bc832517491555
Sergei <ps_minsk@mail.ru>
2018-10-31 23:28:10 +0300

    Add project files.

---
+++ AL3-P13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AL3_P13
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            UniqueItem uI1 = new UniqueItem(5);
+            UniqueItem uI2 = new UniqueItem();
+            Console.WriteLine(uI1.Id);
+            Console.WriteLine(uI2.Id);
+            UniqueItem uI3 = new UniqueItem(200); //т.к. экземпляры класса уже созданы то игнорируем новую нумерацию
+            Console.WriteLine(uI3.Id);            
+        }
+    }    
+}
---
+++ AL3-P13/UniqueItem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AL3_P13
+{
+    class UniqueItem
+    {
+        private static int nextId { get; set; }
+        private static int count = 0;
+        private static int checkExistance { get; set; } // проверяем существование ранее созданного класса
+        public int Id { get; private set; }
+        public UniqueItem()
+        {
+            count++;
+            Id = (int)nextId;
+            nextId++;
+        }
+        public UniqueItem(int first)
+        {            
+            if (count == 0)
+            {
+                Id = first;
+                nextId = first;
+            }
+            else
+                Id = nextId;    
+            nextId++;
+            count++;
+        }
+    }
+}
